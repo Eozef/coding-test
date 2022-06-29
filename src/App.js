920975102f2570b671479fb9c4ff6f5ee4ce8fbe
Eozef <www.github.com/Eozef.com>
2022-06-30 00:27:06 +1000

    code refractory

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from "react";
-
 export default function App() {
   const [number, setNumber] = useState(null);
   useEffect(() => {
     fetch("https://join.reckon.com/test1/rangeInfo")
-      .then((res) => res.json())
+      .then((res1) => res1.json())
       .then((number) => console.log(number))
-      .catch((err) => {
-        console.log(err.message);
-      });
+      .catch((err1) => {
+        console.log(err1.message);
+      })
+      .then(
+        fetch("https://join.reckon.com/test1/divisorInfo")
+          .then((res2) => res2.json())
+          .then((number) => console.log(number))
+          .catch((err2) => {
+            console.log(err2.message);
+          })
+      );
   }, []);
-
-  useEffect(() => {
-    fetch("https://join.reckon.com/test1/divisorInfo")
-      .then((res) => res.json())
-      .then((number) => console.log(number))
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
-  if (!number) {
-    return <div></div>;
-  }
 
   return <App number={number} />;
 }
+
+// export default function App() {
+//   let endPoint1 = fetch("https://join.reckon.com/test1/rangeInfo");
+//   let endPoint2 = fetch("https://join.reckon.com/test1/divisorInfo");
+//   useEffect(() => {
+//     Promise.all([endPoint1, endPoint2])
+//       .then((numbers) => Promise.all(numbers.map((number) => number.json())))
+//       .then((totalNumber) => {
+//         let endPoint1Resp = totalNumber[0];
+//         let endPoint2Resp = totalNumber[1];
+//         let totalEndPointResp = endPoint1Resp + endPoint2Resp;
+//         return totalEndPointResp;
+//       });
+//   }, [endPoint1, endPoint2]);
+
+//   return <App totalEndPointResp />;
+// }
