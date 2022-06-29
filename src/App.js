c50869ba82b32486a1e2940d1580d3e5662c858a
Eozef <www.github.com/Eozef.com>
2022-06-29 23:05:19 +1000

    endpoint 2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ export default function App() {
       });
   }, []);
 
+  useEffect(() => {
+    fetch("https://join.reckon.com/test1/divisorInfo")
+      .then((res) => res.json())
+      .then((number) => console.log(number))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   if (!number) {
     return <div></div>;
   }
